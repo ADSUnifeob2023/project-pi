21538925e9beed7b68e38d90b8922a9b1045cfa0
ADSUnifeob2023 <gabriel.cursino@sou.unifeob.edu.br>
2025-05-30 09:43:10 -0300

    update server for ES(Module)

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
-import path from 'path';
+import { fileURLToPath } from 'url';
+import bodyParser from 'body-parser';
 import express from 'express';
-const bodyParser = require('body-parser');
+import path from 'path';
 
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '../public'))); 
